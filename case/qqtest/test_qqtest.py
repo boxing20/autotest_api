2b51145ffb31d077669130b451285a1c27271465
boxing20 <704466925@qq.com>
2021-06-25 00:54:42 +0800

    commit second case

--- case/qqtest/test_qqtest.py
+++ case/qqtest/test_qqtest.py
@@ -1,16 +1,17 @@
 import pytest
 import requests
 
-#def test_qqtest_01():
-host = "http://japi.juhe.cn"
-path = "/qqevaluate/qq"
-headers = {
-    "Content-Type": "application/json;charset=UTF-8"
-}
-bady = {
-    "key": "eafe307fac13899b5c0c6ff6bc4e5f91",
-    "qq": "704466925"
-}
-r = requests.post(url= host + path, data=bady)
-# print(r.text)
-r.json()["reason"] == []
+def test_qqtest_01():
+    host = "http://japi.juhe.cn"
+    path = "/qqevaluate/qq"
+
+    bady = {
+        "key": "eafe307fac13899b5c0c6ff6bc4e5f91",
+        "qq": "704466925"
+    }
+    r = requests.post(url= host + path, data=bady)
+    print(r.text)
+    r.json()["reason"] == "seccess"
+
+if __name__ == "__main__":
+    pytest.main(["-v", "test_qqtest.py"])
